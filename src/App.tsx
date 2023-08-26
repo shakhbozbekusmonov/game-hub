f0c8474241d070d24c479ef96341d3ad3425cc94
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-26 16:48:14 +0500

    🛠️ Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-
+import { Grid, GridItem } from "@chakra-ui/layout";
+import { Show } from "@chakra-ui/media-query";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <Grid
+            templateAreas={{
+                base: `"nav" "main"`,
+                lg: `"nav nav" "aside main"`,
+            }}>
+            <GridItem area='nav' bg='coral'>
+                Nav
+            </GridItem>
+            <Show above='lg'>
+                <GridItem area='aside' bg='gold'>
+                    Aside
+                </GridItem>
+            </Show>
+            <GridItem area='main' bg='dodgerblue'>
+                Main
+            </GridItem>
+        </Grid>
+    );
+};
 
-export default App
+export default App;
