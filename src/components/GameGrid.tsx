83ad1850aacf2fcaf5e4703c9157205af8da8ec8
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-30 11:22:22 +0500

    🛠️ Fetch url fixed

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,7 @@ const GameGrid = () => {
 
     useEffect(() => {
         apiClient
-            .get<FetchGamesResponse>("/xgames")
+            .get<FetchGamesResponse>("/games")
             .then((res) => setGames(res.data.results))
             .catch((err) => setError(err.message));
     }, []);
