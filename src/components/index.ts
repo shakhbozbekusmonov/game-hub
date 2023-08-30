9486443ca4a9d3871eaebe84830456a09ff9ec5d
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-30 13:10:52 +0500

    🛠️ Building game cards

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as NavBar } from "./NavBar";
 export { default as ColorModeSwitch } from "./ColorModeSwitch";
 export { default as GameGrid } from "./GameGrid";
+export { default as GameCard } from "./GameCard";
