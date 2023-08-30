aa71dfcfd4e9ed0455dd9d3fabb19c8e8a381ba9
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-30 16:12:18 +0500

    Show a spinner while fetching the genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
     const { data, error, isLoading } = useGames();
-    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
     return (
         <>
