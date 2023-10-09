f624660adff9854ab1fd0d34b0e497aa1b980ad7
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-10-09 13:44:18 +0500

    Refactor- move entity interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,19 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import ms from "ms";
 import APIClient from "../services/api-client";
+import { Platform } from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
-export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-}
-
 const usePlatforms = () =>
     useQuery({
         queryKey: ["platforms"],
         queryFn: apiClient.getAll,
-        staleTime: ms("24h"),
+        staleTime: ms("24h"), 
         initialData: platforms,
     });
 
