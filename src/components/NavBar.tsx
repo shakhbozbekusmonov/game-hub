066fbeb4deff3dd1f558a8a7e0e5902d705c8b45
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-10-09 13:39:56 +0500

    Fetch game details

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,13 @@ import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import logo from "../assets/logo.webp";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+    const navigate = useNavigate();
     return (
         <HStack padding='10px'>
-            <Image width='50px' src={logo} />
+            <Image width='50px' onClick={() => navigate("/")} src={logo} />
             <SearchInput />
             <ColorModeSwitch />
         </HStack>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-import { GameDetailPage, HomePage, Layout } from "./pages";
+import { GameDetailPage, HomePage, Layout, ErrorPage } from "./pages";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
+        errorElement: <ErrorPage />,
         children: [
             { index: true, element: <HomePage /> },
-            { path: "games/:id", element: <GameDetailPage /> }, 
+            { path: "games/:slug", element: <GameDetailPage /> },
         ],
     },
 ]);
