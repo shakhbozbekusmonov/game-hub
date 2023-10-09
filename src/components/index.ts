72cdc7cd8e3ec0d8d3c25b66d68cbee42f3c1802
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-10-09 15:00:24 +0500

    Build game trailer

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,3 +10,4 @@ export { default as ExpandableText } from "./ExpandableText";
 export { default as DefinitionItem } from "./DefinitionItem";
 export { default as CriticScore } from "./CriticScore";
 export { default as GameAttributes } from "./GameAttributes";
+export { default as GameTrailer } from "./GameTrailer";
